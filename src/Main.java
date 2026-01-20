4b3f020a5fcdf2c1fc60b039b4d446df20bafc31
eddy <ees5753@psu.edu>
2026-01-20 11:48:17 -0500

    added my ride

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void man(String[] args){
-        Motorcycle m1 = new Motorcycle();
+        Motorcycle m1 = new Motorcycle(color1: "BLUE");
+        System.out.println(m1.getColor());
+
+        Motorcycle m2 = new Motorcycle(color1: "RED");
+        System.out.println(m2.getColor());
+
     }
-}
+
+    }
